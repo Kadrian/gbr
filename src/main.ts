c38988ec27f4171efc2644e387ae4486d59be436
Kai Rollmann <kai.rollmann@gmail.com>
2020-08-02 16:30:48 +0200

    Add some meta fields for npm publishing

--- src/main.ts
+++ src/main.ts
@@ -1,50 +1,50 @@
-import { execSync } from "child_process";
-import { prompt } from "inquirer";
+import { execSync } from 'child_process'
+import { prompt } from 'inquirer'
 
 function getRecentBranchesWithDates() {
   const branchesWithDateCommand =
-    "git for-each-ref --sort=committerdate refs/heads/ --format='%(committerdate:short) %(refname:short)'";
+    "git for-each-ref --sort=committerdate refs/heads/ --format='%(committerdate:short) %(refname:short)'"
 
   return execSync(branchesWithDateCommand)
     .toString()
     .trim()
-    .split("\n")
-    .map((br) => br.trim());
+    .split('\n')
+    .map((br) => br.trim())
 }
 
 function getCurrentBranchRef() {
-  return execSync("git branch --show-current").toString().trim();
+  return execSync('git branch --show-current').toString().trim()
 }
 
 function getCurrentBranchIdx(branches: string[]) {
-  const branchRefs = branches.map((br) => br.split(" ")[1]);
-  const currentBranchRef = getCurrentBranchRef();
+  const branchRefs = branches.map((br) => br.split(' ')[1])
+  const currentBranchRef = getCurrentBranchRef()
 
-  return branchRefs.indexOf(branchRefs.find((br) => br === currentBranchRef));
+  return branchRefs.indexOf(branchRefs.find((br) => br === currentBranchRef))
 }
 
 async function switchBranch() {
-  const branches = getRecentBranchesWithDates();
-  const currentBranchIdx = getCurrentBranchIdx(branches);
+  const branches = getRecentBranchesWithDates()
+  const currentBranchIdx = getCurrentBranchIdx(branches)
 
   const choice = await prompt({
-    name: "Branch",
-    type: "list",
+    name: 'Branch',
+    type: 'list',
     default: currentBranchIdx,
     choices: branches,
     loop: false,
-  });
+  })
 
-  const nextBranch = choice.Branch.split(" ")[1];
+  const nextBranch = choice.Branch.split(' ')[1]
 
-  execSync(`git checkout ${nextBranch}`);
+  execSync(`git checkout ${nextBranch}`)
 }
 
-(async function () {
+;(async function () {
   try {
-    await switchBranch();
+    await switchBranch()
   } catch (e) {
     // All errors in os commands are printed already, so we can simply catch and exit(1)
-    process.exit(1);
+    process.exit(1)
   }
-})();
+})()
